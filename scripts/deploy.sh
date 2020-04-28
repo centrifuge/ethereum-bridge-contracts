a2c384ba5944a29da29258288fb5a567a2c1fa50
vedhavyas <vedhavyas.singareddi@gmail.com>
2020-04-28 19:40:31 +0530

    update deply contract

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,12 +10,13 @@ export ETH_KEYSTORE=${ETH_KEYSTORE:-./keystore}
 export ETH_RPC_URL=${ETH_RPC_URL:-"http://localhost:8545"}
 export ETH_PASSWORD=${ETH_PASSWORD:-"/dev/null"}
 export ETH_FROM=${ETH_FROM:-"0x89b0a86583c4444acfd71b463e0d3c55ae1412a5"}
+export RELY=${RELY:-"0x89b0a86583c4444acfd71b463e0d3c55ae1412a5"}
 
 # deploy asset contracts
 dapp update
 dapp build --extract
 
-assetAddr=$(seth send --create out/BridgeAsset.bin 'BridgeAsset(uint8)' "10" "$ETH_FROM")
+assetAddr=$(seth send --create out/BridgeAsset.bin 'BridgeAsset(uint8)' "10" "$RELY")
 
 jq --arg addr $assetAddr --arg env $CENT_ENV '.[$env].address = $addr' addresses.json > addresses_aux.json
 mv addresses_aux.json addresses.json
