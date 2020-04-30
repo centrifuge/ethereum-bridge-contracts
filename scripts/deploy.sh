9e9c8262de5fc3f128f59666876fc8f1238a7407
Charly <charly@centrifuge.io>
2020-04-30 13:51:41 +0200

    remove default RELY and ETH_FROM addresses

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,8 @@ export ETH_GAS=${ETH_GAS:-4712388}
 export ETH_KEYSTORE=${ETH_KEYSTORE:-./keystore}
 export ETH_RPC_URL=${ETH_RPC_URL:-"http://localhost:8545"}
 export ETH_PASSWORD=${ETH_PASSWORD:-"/dev/null"}
-export ETH_FROM=${ETH_FROM:-"0x89b0a86583c4444acfd71b463e0d3c55ae1412a5"}
-export RELY=${RELY:-"0x89b0a86583c4444acfd71b463e0d3c55ae1412a5"}
+export ETH_FROM=$ETH_FROM
+export RELY=$RELY
 
 # deploy asset contracts
 dapp update
